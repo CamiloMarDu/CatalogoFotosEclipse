ceccc82fdce236cc8be0538b9076f47a9f145eb1
juanc <juanc@messi>
2024-04-11 19:24:40 -0500

    sfad

--- Taller3Catalogo/src/control/Gestor.java
+++ Taller3Catalogo/src/control/Gestor.java
@@ -5,11 +5,14 @@ import vista.WCatalogo;
 //Importación de clases ajenas
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Gestor implements ActionListener{
-
+	File[] files;
 	private WCatalogo catalogo;
 	public Gestor() {
 		catalogo= new WCatalogo();
@@ -40,6 +43,40 @@ public class Gestor implements ActionListener{
 			
 			
 		}
+	}
+	public void ImageFolderChooser() {
+		File[] files;
+	        // Crear un JFileChooser
+	        JFileChooser fileChooser = new JFileChooser();
+
+	        // Configurar el JFileChooser para que solo muestre directorios
+	        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+	        // Mostrar el cuadro de diálogo de selección de archivos
+	        int result = fileChooser.showOpenDialog(null);
+
+	        if (result == JFileChooser.APPROVE_OPTION) {
+	            // Obtener la carpeta seleccionada
+	            File selectedFolder = fileChooser.getSelectedFile();
+
+	            // Obtener todos los archivos en la carpeta seleccionada
+	             files = selectedFolder.listFiles();
+
+	            if (files != null) {
+	                // Filtrar solo los archivos de imagen (por ejemplo, jpg, png, etc.)
+	                FileNameExtensionFilter filter = new FileNameExtensionFilter(
+	                        "Imagenes", "jpg", "jpeg", "png", "gif");
+	               
+	             
+	            } else {
+	                System.out.println("La carpeta seleccionada está vacía.");
+	               
+	            }
+	        } else {
+	            System.out.println("Operación cancelada por el usuario.");
+	        
+	        }
+	 
 	}
 
 }
