1f1026471e05cbbbc2223e6e6fd4d59304647a6b
Camilo <camilo.mardu@gmail.com>
2024-04-13 17:35:12 -0500

    Creacion clase ventana del visor de imagen

--- Taller3Catalogo/src/control/Gestor.java
+++ Taller3Catalogo/src/control/Gestor.java
@@ -11,16 +11,18 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class Gestor implements ActionListener{
+
+public class Gestor implements ActionListener {
 	File[] files;
 	private WCatalogo catalogo;
+
 	public Gestor() {
-		catalogo= new WCatalogo();
+		catalogo = new WCatalogo();
 		catalogo.setVisible(true);
 		catalogo.setResizable(false);
 		catalogo.setLocationRelativeTo(null);
 		catalogo.setTitle("Catálogo de Imágenes");
-		
+
 		// SECCION DE ACTION LISTENERS DE LOS BOTONES
 		this.catalogo.getBtnBuscarDirectorio().addActionListener(this);
 		this.catalogo.getBtnBuscarDirectorio().setActionCommand("buscarCarpeta");
@@ -28,58 +30,61 @@ public class Gestor implements ActionListener{
 		this.catalogo.getBtnSalir().addActionListener(this);
 		this.catalogo.getBtnSalir().setActionCommand("salir");
 	}
-	
+
 	public void cerrarVentana(int val) {
 		if (val == JOptionPane.YES_NO_OPTION) {
-			System.exit(0);			
+			System.exit(0);
 		}
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (("salir").equals(e.getActionCommand())) {
+
+		switch (e.getActionCommand()) {
+		case "salir":
 			cerrarVentana(this.catalogo.avisoCerrarVentana());
-			
-		} else if (("buscarCarpeta").equals(e.getActionCommand())) {
-			
-			
+			break;
+		case "buscarCarpeta":
+
+			break;
 		}
 	}
+
 	public void ImageFolderChooser() {
 		File[] files;
-	        // Crear un JFileChooser
-	        JFileChooser fileChooser = new JFileChooser();
+		// Crear un JFileChooser
+		JFileChooser fileChooser = new JFileChooser();
 
-	        // Configurar el JFileChooser para que solo muestre directorios
-	        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		// Configurar el JFileChooser para que solo muestre directorios
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
-	        // Mostrar el cuadro de diálogo de selección de archivos
-	        int result = fileChooser.showOpenDialog(null);
+		// Mostrar el cuadro de diálogo de selección de archivos
+		int result = fileChooser.showOpenDialog(null);
 
-	        if (result == JFileChooser.APPROVE_OPTION) {
-	            // Obtener la carpeta seleccionada
-	            File selectedFolder = fileChooser.getSelectedFile();
+		if (result == JFileChooser.APPROVE_OPTION) {
+			// Obtener la carpeta seleccionada
+			File selectedFolder = fileChooser.getSelectedFile();
 
-	            // Obtener todos los archivos en la carpeta seleccionada
-	             files = selectedFolder.listFiles();
+			// Obtener todos los archivos en la carpeta seleccionada
+			files = selectedFolder.listFiles();
 
-	            if (files != null) {
-	                // Filtrar solo los archivos de imagen (por ejemplo, jpg, png, etc.)
-	                FileNameExtensionFilter filter = new FileNameExtensionFilter(
-	                        "Imagenes", "jpg", "jpeg", "png", "gif");
-	               
-	             
-	            } else {
-	                System.out.println("La carpeta seleccionada está vacía.");
-	               
-	            }
-	        } else {
-	            System.out.println("Operación cancelada por el usuario.");
-	        
-	        }
-	 
+			if (files != null) {
+				// Filtrar solo los archivos de imagen (por ejemplo, jpg, png, etc.)
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("Imagenes", "jpg", "jpeg", "png", "gif");
+
+			} else {
+				System.out.println("La carpeta seleccionada está vacía.");
+
+			}
+		} else {
+			System.out.println("Operación cancelada por el usuario.");
+
+		}
+
 	}
+
 	public double progreso(File[] files) {
-		double porcentaje=100/files.length;
+		double porcentaje = 100 / files.length;
 		return porcentaje;
 	}
 
---
+++ Taller3Catalogo/src/vista/WReproductor.java
@@ -0,0 +1,116 @@
+package vista;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import control.Gestor;
+
+import javax.swing.JButton;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import javax.swing.JProgressBar;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+import java.awt.Color;
+
+public class WReproductor extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	
+	private JProgressBar barra;
+	private JPanel panelBotonFoto;
+	private JButton btnDetener;
+	private JButton btnContinuar;
+	private JButton btnSalir;
+	private JTextField fieldNumeroImagen;
+	private JLabel lblSlash;
+	
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WReproductor frame = new WReproductor();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	/**
+	 * Create the frame.
+	 */
+	public WReproductor() {
+		iniciar();
+	}
+
+	public void iniciar() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 650, 800);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		panelBotonFoto = new JPanel();
+		panelBotonFoto.setBounds(16, 100,600, 600);
+		contentPane.add(panelBotonFoto);
+		
+		btnDetener = new JButton("DETENER");
+		btnDetener.setFont(new Font("Roboto", Font.BOLD, 17));
+		btnDetener.setBounds(0, 710, 260, 50);
+		contentPane.add(btnDetener);
+		
+		btnContinuar = new JButton("CONTINUAR");
+		btnContinuar.setFont(new Font("Roboto", Font.BOLD, 17));
+		btnContinuar.setBounds(260, 710, 260, 50);
+		contentPane.add(btnContinuar);
+		
+		btnSalir = new JButton("SALIR");
+		btnSalir.setFont(new Font("Roboto", Font.BOLD, 17));
+		btnSalir.setBounds(520, 710, 115, 50);
+		contentPane.add(btnSalir);
+		
+		barra = new JProgressBar();
+		barra.setForeground(new Color(0, 0, 0));
+		barra.setBounds(16, 69, 600, 20);
+		barra.setStringPainted(true);
+		contentPane.add(barra);
+		
+		fieldNumeroImagen = new JTextField();
+		fieldNumeroImagen.setBackground(new Color(255, 255, 255));
+		fieldNumeroImagen.setHorizontalAlignment(SwingConstants.RIGHT);
+		fieldNumeroImagen.setFont(new Font("Roboto", Font.PLAIN, 15));
+		fieldNumeroImagen.setBounds(485, 45, 60, 20);
+		fieldNumeroImagen.setEditable(false);
+		contentPane.add(fieldNumeroImagen);
+		
+		JLabel lblNumeroImagenes = new JLabel("");
+		lblNumeroImagenes.setFont(new Font("Roboto", Font.PLAIN, 15));
+		lblNumeroImagenes.setForeground(new Color(0, 0, 0));
+		lblNumeroImagenes.setBounds(556, 45, 60, 20);
+		contentPane.add(lblNumeroImagenes);
+		
+		lblSlash = new JLabel("/");
+		lblSlash.setForeground(new Color(0, 0, 0));
+		lblSlash.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblSlash.setBounds(546, 45, 10, 20);
+		contentPane.add(lblSlash);
+		
+		
+		
+	}
+}
